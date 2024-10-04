b309905052480762515e2a6b37b422dbb6bb821b
Nelstan <105560923+nelstan@users.noreply.github.com>
2024-10-04 11:17:52 +0500

    first commit

---
+++ chat wa/main.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("button-sign-up");
+const buttonLogin = document.getElementById('button-login');
+button.addEventListener("click", () => {
+    window.location.href = "./chat.html";
+})
+
+buttonLogin.addEventListener("click", () =>{
+    window.location.href = "./login.html";
+})
+ 
